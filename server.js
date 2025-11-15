6d739de5e49d1c1c596f95659cc03f473c05f037
Samih Ouague <41809893+SamihOuague@users.noreply.github.com>
2025-11-16 00:29:22 +0100

    Add console log for server start message

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ let cors = require("cors");
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
+app.listen(3000, () => console.log("http://localhost:3000/"));
